d3b55f7a792705047d294d4f3f1556cbb8e064dc
kazuya <sbvfkku0985@gmail.com>
2025-08-24 04:33:56 +0900

    feat(admin): BooksのCRUDを実装（一覧・新規・編集・削除とエラーメッセージ表示、管理レイアウト導線）

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,13 +1,43 @@
 class Admin::BooksController < Admin::BaseController
+  layout "admin"
+
   def index
-    head :ok
+    @books = Book.order(updated_at: :desc).page(params[:page])
   end
 
   def new
-    head :ok
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to admin_books_path, notice: "作成しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
-    head :ok
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to admin_books_path, notice: "更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Book.find(params[:id]).destroy
+    redirect_to admin_books_path, notice: "削除しました"
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :description)
   end
 end
