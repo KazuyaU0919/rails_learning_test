ea599d05d16abef64666990cffff04f93b3c629e
kazuya <sbvfkku0985@gmail.com>
2025-09-26 10:50:24 +0900

    エラー修正

--- app/helpers/pre_codes_helper.rb
+++ app/helpers/pre_codes_helper.rb
@@ -1,2 +1,9 @@
+# app/helpers/pre_codes_helper.rb
 module PreCodesHelper
+  ALLOWED_TAGS = %w[b i em strong code pre br p ul ol li a].freeze
+  ALLOWED_ATTRS = %w[href].freeze
+
+  def sanitized_html(html)
+    sanitize(html, tags: ALLOWED_TAGS, attributes: ALLOWED_ATTRS)
+  end
 end
